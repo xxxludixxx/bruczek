b08a3595bda196a556eecc671c295dc6f5190f22
Ludi <kubalu2@gmail.com>
2016-06-26 17:42:33 +0200

    [Bruczek-theme] Create footer - in progress.

--- wp-content/themes/bruczek-theme/footer.php
+++ wp-content/themes/bruczek-theme/footer.php
@@ -1,3 +1,32 @@
+<footer class="site-footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3">
+                <h6>Nasza siedziba:</h6>
+                <p>www.bruczek.pl</p>
+                <p>ul. Kostki brukowej 123</p>
+                <p>22-361 Warszawa</p>
+            </div>
+            <div class="col-md-3">
+                <h6>Zapytaj o wycenę:</h6>
+                <br>
+                <p>Zostaw swoje zgłoszenie, a my skontaktujemy się z Tobą.</p>
+            </div>
+            <div class="col-md-offset-3 col-md-3">
+                <h4>Znajdź nas w internecie:</h4>
+                <div class="social-icon-wrapper">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-offset-5 col-md-2">
+                <p>&#9400; 2016 www.bruczek.pl</p>
+            </div>
+        </div>
+    </div>
+
+</footer>
+
 <?php
 /**
  * Created by PhpStorm.
@@ -7,4 +36,4 @@
  */
 ?>
 <?php wp_footer(); ?>
-</body
+</body>
